97dd79c31bcc8575af696f9c2faf8de379b59b9b
thomas <bezille.thomasb@gmail.com>
2024-05-14 10:10:06 +0200

    Intégration statique du widget

--- src/components/WeatherWidget/WeatherWidget.jsx
+++ src/components/WeatherWidget/WeatherWidget.jsx
@@ -1,7 +1,17 @@
 import './WeatherWidger.scss';
 
 const WeatherWidger = () => {
-  return <div>Ici bientôt la météo</div>;
+  return (
+    <article className="weather-widget">
+      <div className="weather-container">
+        <div className="weather-infos">
+          <h3 className="weather-city">Nantes</h3>
+          <p className="weather-zipcode">44000</p>
+        </div>
+        <div className="weather-temperature">20°</div>
+      </div>
+    </article>
+  );
 };
 
 export default WeatherWidger;
